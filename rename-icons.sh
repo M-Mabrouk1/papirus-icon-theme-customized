28efa33436310cdc50f849ff71e19b29217d0890
M-Mabrouk1 <MuhammadMabrouk@gmail.com>
2022-04-12 05:10:57 +0200

    updated vlc, virt-manager and obs

---
+++ rename-icons.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+
+# Set with the flags "-e", "-u","-o pipefail" cause the script to fail
+# if certain things happen, which is a good thing.  Otherwise, we can
+# get hidden bugs that are hard to discover.
+set -euo pipefail
+
+name=obs.svg
+icons=$(find -type f -name $name)
+
+for x in ${icons}
+do
+    mv "${x}" "${x}".bak
+    echo "Moving ${x}"
+done
